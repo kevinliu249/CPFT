f5f61a8db76af4a0fcefe7727372a6172c254d87
kevinliu249 <kevinliu240@gmail.com>
2025-02-14 21:26:37 -0800

    Initial Workout Logging API Implemented

--- backend/controllers/workout_controller.py
+++ backend/controllers/workout_controller.py
@@ -2,7 +2,7 @@
 # Daily Workout
 
 from flask import Blueprint, request, jsonify
-from services.workout_service import create_workout_service
+from services.workout_service import create_workout_service, store_workout_service, log_workout_service, retrieve_workout_service
 from services.fitness_plan_service import get_fitness_plan
 
 
@@ -29,8 +29,39 @@ def workout_creation_route():
         if error:
             return jsonify({"message": error}), 400
         
+        # Call store workout service to store in database
+        store_workout = store_workout_service(user_name, workout)
+        
         # If workout creation is successful, return workout
-        return jsonify({"username": user_name, "message": "Workout created successfully", "workout": workout}), 200
+        return jsonify({"username": user_name, "message": "Workout created successfully and stored", "workout": workout}), 200
+
+    except Exception as e:
+        return jsonify({"message": "Internal server error", "error": str(e)}), 500
+
+# Route for workout storing
+@workout_controller.route('/workout', methods=['POST'])
+def workout_logging_route():
+    try:
+
+        # Get the username from query parameters
+        user_name = request.args.get('username')
+        
+        if not user_name:
+            return jsonify({"message": "Username is required"}), 400
+        
+        workout = retrieve_workout_service(user_name)
+        
+        # Get JSON data from the request body
+        data = request.get_json()
+
+        # Call store workout service
+        stored_workout = log_workout_service(user_name, data, workout)
+
+        if stored_workout != True:
+            return jsonify({"message": stored_workout}), 400
+        
+        # If workout storing is successful, return message
+        return jsonify({"username": user_name, "message": "Workout stored successfully", "Daily Workout": workout, "Completed Input": data}), 200
 
     except Exception as e:
         return jsonify({"message": "Internal server error", "error": str(e)}), 500
--- backend/services/workout_service.py
+++ backend/services/workout_service.py
@@ -5,9 +5,11 @@
 
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
+from datetime import datetime
 from services.workout_algorithm import generate_workout
+from models.workout_model import store_workout_model, retrieve_workout_model, log_workout_model
 
-
+# Create Daily Workout
 def create_workout_service(user_name, fitness_plan):
     from app import mongo
 
@@ -16,3 +18,59 @@ def create_workout_service(user_name, fitness_plan):
 
     # Return the newly created workout, no errors
     return result, None
+
+
+# Store Daily workout that was generated
+def store_workout_service(user_name, workout):
+    from app import mongo
+
+    # Check if daily workout already exists
+    existing_plan = mongo.db.dailyworkout.find_one({"username": user_name})
+    if existing_plan:
+        # If a workout exists, delete the existing entry
+        mongo.db.dailyworkout.delete_one({"username": user_name})
+
+    # Call store workout model to store in MongoDB
+    store_workout_model(user_name, workout)
+    return
+
+
+# Retrieve Daily Workout that was generated
+def retrieve_workout_service(user_name):
+    from app import mongo
+    workout = retrieve_workout_model(user_name)
+    return workout
+
+
+# Log Workout Data sent from frontend
+def log_workout_service(user_name, data, workout):
+
+    current_datetime = datetime.now()
+    # Log the date and time in a specific format
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+    first_exercise_data = {
+        "username": user_name,
+        "name": workout["workout"][0]['name'],
+        "target": workout["workout"][0]['target'],
+        "sets": data[0]["sets"],
+        "reps": data[0]["reps"],
+        "weight": data[0]["weight"],
+        "time": data[0]["time"],
+        "datetime": formatted_datetime
+    }
+
+    second_exercise_data = {
+        "username": user_name,
+        "name": workout["workout"][1]['name'],
+        "target": workout["workout"][1]['target'],
+        "sets": data[1]["sets"],
+        "reps": data[1]["reps"],
+        "weight": data[1]["weight"],
+        "time": data[1]["time"],
+        "datetime": formatted_datetime
+    }
+    # Call log workout model to store into mongoDB
+    log_workout_model(first_exercise_data, second_exercise_data)
+
+    return True
