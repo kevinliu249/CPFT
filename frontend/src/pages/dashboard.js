af273164b2ab421ad80c89621e8b612c4c3fee4b
Elliot Clark <etcclark@gmail.com>
2025-02-22 18:29:52 -0700

    Added User Avatar Selection to Dashboard

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -3,16 +3,29 @@ import { useNavigate } from "react-router-dom";
 import { GlobalStateContext } from "../context/GlobalStateContext";
 import '../styles/App.css';
 import '../styles/Dashboard.css';
+import avatar1 from "../images/avatar1.png";
+import avatar2 from "../images/avatar2.png";
+import avatar3 from "../images/avatar3.png";
+import avatar4 from "../images/avatar4.png";
 
-const Dashboard = ({ username }) => {
+const Dashboard = ({ username, avatarImageNum }) => {
   const navigate = useNavigate();
   const { theme } = useContext(GlobalStateContext);
+
+  const avatars = {
+    1: avatar1,
+    2: avatar2,
+    3: avatar3,
+    4: avatar4,
+  };
 
   return (
     <div className={`Dashboard ${theme}`}>
       <div id="dashboardContainer"> {/* Added container for styling consistency */}
         <h1>Dashboard</h1>
         <h2>Welcome {username}</h2>
+        <img alt="Avatar" src={avatars[avatarImageNum]}></img>
+
         <hr />
         <p>No Pain, No Gainz! You Got This!</p>
 
